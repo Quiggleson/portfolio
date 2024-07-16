97d2f015b7cc565eb163d3906cbe98468964aed4
Robert Quigley <robertquigley@oakland.edu>
2024-07-16 14:48:00 -0400

    add gh pages home

--- app/topnav.tsx
+++ app/topnav.tsx
@@ -4,16 +4,16 @@ export default function TopNav() {
     
     const buttons = [
         // Warning: home will link incorrectly on github pages
-        {name: 'Home', link: '/'},
+        {name: 'Localhost Home', link: '/'},
         {name: '3SAT', link: 'sat'},
-        {name: 'Example', link: '/'},
+        {name: 'GH Pages Home', link: '/portfolio'},
         {name: 'Example', link: '/'},
     ];
 
     return (
         <div className="w-screen h-14 border-b border-black sticky mx-auto">
-            <div className="w-1/2 float-right flex justify-between mx-5">
-            {buttons.map((button, i) => <a key={i} className="rounded p-2 m-2 mx-6 hover:bg-sky-500" href={button.link}>{button.name}</a>)}
+            <div className="float-right flex justify-between mx-5">
+            {buttons.map((button, i) => <a key={i} className="rounded p-2 m-2 mx-6 hover:bg-sky-500 text-nowrap" href={button.link}>{button.name}</a>)}
             </div>
         </div>
     );
