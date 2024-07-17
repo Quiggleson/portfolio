2d6cbfa86fed14e71dfe16dd1f72140eaeee38af
Robert Quigley <robertquigley@oakland.edu>
2024-07-16 22:31:01 -0400

    add escapes

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,15 @@ export default function Home(){
       <p>This website is a collection of things I find cool</p>
     </div>
     <div className="m-2 mx-4 text-justify">
-      <h1 className="font-bold">What's all this then?</h1>
-      <p>This is a collection of things I find cool. It contains various items such as some papers I've worked on, some things I find interesting, and some beautiful music to name a few. I am most excited about the 3SAT program I'm working on which allows one to input descriptions of clauses and it will return helpful information about the implication graph. </p>
-      <h1 className="font-bold">What's next?</h1>
+      <h1 className="font-bold">What&apos;s all this then?</h1>
+      <p>This is a collection of things I find cool. It contains various items such as some papers I&apos;ve worked on, some things I find interesting, and some beautiful music to name a few. I am most excited about the 3SAT program I&apos;m working on which allows one to input descriptions of clauses and it will return helpful information about the implication graph. </p>
+      <h1 className="font-bold">What&apos;s next?</h1>
       <ul>
         <li>Finish 3SAT page</li>
         <li>Upload Refutation Paper</li>
         <li>About page</li>
       </ul>
-      <h1 className="font-bold">When's all this then?</h1>
+      <h1 className="font-bold">When&apos;s all this then?</h1>
       <p>In case I get very tired and stop working on this, this website was under development July 2024.</p>
     </div>
     <div className="mx-2">
