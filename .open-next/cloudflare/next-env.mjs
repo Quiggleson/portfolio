2ccce122df39d45f8ad01fccc9dab10048075a6a
Robert Quigley <robertquigley@oakland.edu>
2025-08-05 15:13:25 -0400

    add cloudflare support

---
+++ .open-next/cloudflare/next-env.mjs
@@ -0,0 +1,3 @@
+export const production = {"NEXT_PUBLIC_ROOT_PATH":"","SESSION_SECRET":"b2tPljuYrCkx+pdOWCqTXrHSxyad8XGtOzxWEI/Emx0="};
+export const development = {"NEXT_PUBLIC_ROOT_PATH":"","SESSION_SECRET":"b2tPljuYrCkx+pdOWCqTXrHSxyad8XGtOzxWEI/Emx0="};
+export const test = {};
