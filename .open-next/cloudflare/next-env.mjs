086522bad502e21da518217e5cae028d9c31ba54
Robert Quigley <robertquigley@oakland.edu>
2025-08-05 15:38:29 -0400

    add domain name to wrangler.jsonc

--- .open-next/cloudflare/next-env.mjs
+++
@@ -1,3 +0,0 @@
-export const production = {"NEXT_PUBLIC_ROOT_PATH":"","SESSION_SECRET":"b2tPljuYrCkx+pdOWCqTXrHSxyad8XGtOzxWEI/Emx0="};
-export const development = {"NEXT_PUBLIC_ROOT_PATH":"","SESSION_SECRET":"b2tPljuYrCkx+pdOWCqTXrHSxyad8XGtOzxWEI/Emx0="};
-export const test = {};
